598d7ced1772470dae553c217b60db880ec0edd5
Daniel Waterworth <da.waterworth@gmail.com>
2016-07-13 20:31:41 +0100

    Problem parsing

--- main.rs
+++ main.rs
@@ -1,5 +1,12 @@
+#[macro_use]
+extern crate nom;
+
 mod problem;
 mod solver;
+mod parser;
+
+use std::fs::File;
+use std::io::Read;
 
 use problem::Quantifier;
 use problem::Expression;
@@ -9,6 +16,11 @@ use solver::Solution;
 use solver::solve;
 
 fn main() {
+    let mut f = File::open("input.qbf").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+    println!("{:?}", parser::parse(s.as_ref()));
+
     let n = Expression::Var(0);
     let m = Expression::Var(1);
     let e = Expression::And(&n, &m);
---
+++ parser.rs
@@ -0,0 +1,188 @@
+use nom::eof;
+use nom::alpha;
+use nom::space;
+use nom::multispace;
+use nom::IResult;
+
+use problem::Quantifier;
+
+#[derive(Debug)]
+pub struct Literal {
+    polarity: bool,
+    var: String
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    And(Literal, Literal),
+    Or(Literal, Literal),
+    Not(Literal),
+    Lit(Literal),
+    True,
+    False
+}
+
+#[derive(Debug)]
+pub struct Statement {
+    name: String,
+    exp: Expression
+}
+
+fn string_from_slice(slice: &[u8]) -> String {
+    let mut v = Vec::new();
+    v.extend_from_slice(slice);
+    String::from_utf8(v).unwrap()
+}
+
+named!(positive_literal<&[u8], Literal >,
+    chain!(
+        name: alpha,
+
+        ||{Literal {var: string_from_slice(name), polarity: true}}
+    )
+);
+
+named!(negative_literal<&[u8], Literal >,
+    chain!(
+        tag!("~") ~
+        name: alpha,
+
+        ||{Literal {var: string_from_slice(name), polarity: false}}
+    )
+);
+
+named!(literal<&[u8], Literal >,
+    alt!(
+        positive_literal |
+        negative_literal
+    )
+);
+
+named!(and<&[u8], Expression >,
+    chain!(
+        tag!("and") ~
+        tag!("(") ~
+        lhs: literal ~
+        tag!(",") ~
+        opt!(space) ~
+        rhs: literal ~
+        tag!(")"),
+
+        ||{Expression::And(lhs, rhs)}
+    )
+);
+
+named!(or<&[u8], Expression >,
+    chain!(
+        tag!("or") ~
+        tag!("(") ~
+        lhs: literal ~
+        tag!(",") ~
+        opt!(space) ~
+        rhs: literal ~
+        tag!(")"),
+
+        ||{Expression::Or(lhs, rhs)}
+    )
+);
+
+named!(not<&[u8], Expression >,
+    chain!(
+        tag!("not") ~
+        tag!("(") ~
+        exp: literal ~
+        tag!(")"),
+
+        ||{Expression::Not(exp)}
+    )
+);
+
+named!(true_<&[u8], Expression >,
+    chain!(
+        tag!("true"),
+
+        ||{Expression::True}
+    )
+);
+
+named!(false_<&[u8], Expression >,
+    chain!(
+        tag!("true"),
+
+        ||{Expression::False}
+    )
+);
+
+named!(literal_expr<&[u8], Expression >,
+    chain!(
+        lit: literal,
+        ||{Expression::Lit(lit)}
+    )
+);
+
+named!(expression<&[u8], Expression >,
+    alt!(and | or | not | true_ | false_ | literal_expr)
+);
+
+named!(statement<&[u8], Statement >,
+    chain!(
+        name: alpha ~
+        opt!(space) ~
+        tag!("=") ~
+        opt!(space) ~
+        exp: expression ~
+        multispace,
+
+        ||{Statement { name: string_from_slice(name), exp: exp }}
+    )
+);
+
+named!(forall_<&[u8], Quantifier >,
+    chain!(
+        tag!("forall"),
+
+        ||{Quantifier::ForAll}
+    )
+);
+
+named!(exists_<&[u8], Quantifier >,
+    chain!(
+        tag!("exists"),
+
+        ||{Quantifier::Exists}
+    )
+);
+
+named!(quantifier<&[u8], (Quantifier, String) >,
+    chain!(
+        quantifier:
+            alt!(
+                forall_ |
+                exists_
+            ) ~
+        multispace ~
+        name: alpha ~
+        multispace,
+
+        || {(quantifier, string_from_slice(name))}
+    )
+);
+
+named!(file<&[u8], (Vec<(Quantifier, String)>, Vec<Statement>, Expression) >,
+    chain!(
+        quantifiers: many0!(quantifier) ~
+        statements: many0!(statement) ~
+        output: expression ~
+        multispace ~
+        eof,
+
+        ||{(quantifiers, statements, output)}
+    )
+);
+
+pub fn parse(input: &[u8]) -> (Vec<(Quantifier, String)>, Vec<Statement>, Expression) {
+    match file(input) {
+        IResult::Done(_, o) => o,
+        o => panic!("failure to parse: {:?}", o)
+    }
+}
